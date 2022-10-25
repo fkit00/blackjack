8aef19bed22d0fea569059cf9b8f934edb38f1b4
Fiona <100845392+fkit00@users.noreply.github.com>
2022-10-25 15:10:02 +0000

    Used and or instead of an and and got stuck in an infinite loop- now looking for a mystery None

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random 
+import clear
 ############### Our Blackjack House Rules #####################
 
 ## The deck is unlimited in size. 
@@ -21,18 +22,6 @@ def deal_card():
   card=random.choice(cards)
   return card
 
-for _ in range(2):
-  new_cards= deal_card()
-  user_cards.append(new_cards)
-  
-for _ in range(2):
-  new_cards= deal_card()
-  computer_cards.append(new_cards)
-
-  
-print(f"Your cards are {user_cards}")
-print(f"The computer has {computer_cards[0]}")
-
 def calculate_score(cards):
   sum_cards=sum(cards)
   if sum_cards==21:
@@ -46,27 +35,66 @@ def calculate_score(cards):
   return sum_cards
   
 
+for _ in range(2):
+  new_cards= deal_card()
+  user_cards.append(new_cards)
+  
+for _ in range(2):
+  new_cards= deal_card()
+  computer_cards.append(new_cards)
+
+
 user_cards_score = calculate_score(user_cards)
 computer_cards_score=calculate_score(computer_cards)
-print(f"Your score is {user_cards_score}")
-print(f"The computer score is {computer_cards_score}")
 
-if computer_cards_score == 0 or user_cards_score == 0 or user_cards_score>21 :
-  game_over=True
-else:
-  new_card=input(f"Your score is {user_cards_score}, would you like to pick another card? y/n  ")
-  if new_card=='y':
-    additional_card=deal_card()
-    user_cards.append(additional_card)
-    user_cards_score = calculate_score(user_cards)
-    print(f"you picked {additional_card}, your score is {user_cards_score}")
+while not game_over:
+    
+  print(f"Your cards are {user_cards}, your score is {calculate_score(user_cards)}")
+  print(f"The computers first card is  {computer_cards[0]}")
+  
+  if computer_cards_score == 0 or user_cards_score == 0 or user_cards_score>21 :
+    game_over=True
+  else:
+    new_card=input(f"Your score is {user_cards_score}, would you like to pick another card? y/n  ")
+    if new_card=='y':
+      additional_card=deal_card()
+      user_cards.append(additional_card)
+      user_cards_score = calculate_score(user_cards)
+      print(f"you picked {additional_card}")
+    if new_card=='n':
+      game_over=True
+
+while computer_cards_score !=0 and computer_cards_score<17:
+  additional_card=deal_card()
+  computer_cards.append(additional_card)
+  computer_cards_score=calculate_score(computer_cards)
+  print(computer_cards_score)
+
+
+
+
+def compare(computer_cards_score, user_cards_score):
+  if computer_cards_score==user_cards_score:
+    return "It's a draw!"
+  elif computer_cards_score == 0:
+    return "Lose, the house has Blackjack!"
+  elif user_cards_score == 0:
+    return 'Blackjack, you win!'
+  elif user_cards_score>21:
+    return 'You went over, you lose!'
+  elif computer_cards_score>21:
+    return 'House went over, you win!'
+  elif user_cards_score>computer_cards_score:
+    return 'You win!'
+  else: 
+    "you lose"
+
+print(compare(computer_cards_score, user_cards_score))
+
+again= input('Do you want to play again? y/n ')
+
+if again == 'y':
+  clear()
+  game_over=False
 
   
-
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
